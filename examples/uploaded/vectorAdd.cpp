ee711a8794ab6f3bbc4182bf52d71bc8942abc09
Thanos <tstratiko@gmail.com>
2022-08-04 23:01:26 +0300

    Cleared uploaded files

--- examples/uploaded/vectorAdd.cpp
+++
@@ -1,5 +0,0 @@
-void vectorAdd(int[] a, int[] b, int[] c, int size) {
-    for (int i = 0; i < size; i++) {
-        c[i] = a[i] + b[i];
-    }
-}
--- examples/uploaded/vectorAdd.java
+++
@@ -1,5 +0,0 @@
-private static void vectorAdd(int[] a, int[] b, int[] c) {
-    for (@Parallel int i = 0; i < c.length; i++) {
-        c[i] = a[i] + b[i];
-    }
-}
