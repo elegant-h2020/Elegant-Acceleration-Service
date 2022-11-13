3dbd9ff7c2b681e392586413637c65689d0ba57b
Thanos <tstratiko@gmail.com>
2022-11-13 15:56:52 +0000

    [wip] Add String variables for the TornadoVM environment

---
+++ src/main/java/uk/ac/manchester/acceleration/service/elegant/controller/EnvironmentVariables.java
@@ -0,0 +1,9 @@
+package uk.ac.manchester.acceleration.service.elegant.controller;
+
+public class EnvironmentVariables {
+    public static final String GENERATED_KERNELS_DIR = "GENERATED_KERNELS_DIR";
+    public static final String TORNADO_SDK = "TORNADO_SDK";
+    public static final String TORNADOVM_DIR = "TORNADOVM_DIR";
+    public static final String SERVICE_DIR = "SERVICE_DIR";
+    public static final String BOILERPLATE_DIR = "BOILERPLATE_DIR";
+}
