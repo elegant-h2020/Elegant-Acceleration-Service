52747da7d90ac33386207e244c3ff0523dd34986
Thanos <tstratiko@gmail.com>
2022-12-01 10:35:00 +0000

    [feat] Add new class to create JSON contents

---
+++ src/main/java/uk/ac/manchester/acceleration/service/elegant/controller/JsonGenerator.java
@@ -0,0 +1,61 @@
+package uk.ac.manchester.acceleration.service.elegant.controller;
+
+import java.util.Arrays;
+
+public class JsonGenerator {
+
+    public static StringBuilder stringBuilder;
+
+    private static void emitOpeningBrace(StringBuilder sb) {
+        sb.append("{");
+        sb.append("\n");
+    }
+
+    private static void emitClosingBrace(StringBuilder sb) {
+        sb.append("}");
+    }
+
+    private static void emitKeyValuePair(StringBuilder sb, String key, String value, boolean isString, boolean isLastField) {
+        sb.append("  \"");
+        sb.append(key);
+        sb.append("\"");
+        sb.append(": ");
+        if (isString) {
+            sb.append("\"");
+        }
+        sb.append(value);
+        if (isString) {
+            sb.append("\"");
+        }
+        if (!isLastField) {
+            sb.append(",");
+        }
+        sb.append("\n");
+    }
+
+    public static String createJsonContents(Object object) {
+        stringBuilder = new StringBuilder();
+
+        if (object instanceof DeviceInfo) {
+            DeviceInfo deviceInfo = (DeviceInfo) object;
+            emitOpeningBrace(stringBuilder);
+            emitKeyValuePair(stringBuilder, "deviceName", deviceInfo.getDeviceName(), true, false);
+            emitKeyValuePair(stringBuilder, "doubleFPSupport", String.valueOf(deviceInfo.getDoubleFPSupport()), false, false);
+            emitKeyValuePair(stringBuilder, "maxWorkItemSizes", Arrays.toString(deviceInfo.getMaxWorkItemSizes()), false, false);
+            emitKeyValuePair(stringBuilder, "deviceAddressBits", String.valueOf(deviceInfo.getDeviceAddressBits()), false, false);
+            emitKeyValuePair(stringBuilder, "deviceType", deviceInfo.getDeviceType(), true, false);
+            emitKeyValuePair(stringBuilder, "deviceExtensions", deviceInfo.getDeviceExtensions(), true, false);
+            emitKeyValuePair(stringBuilder, "availableProcessors", String.valueOf(deviceInfo.getAvailableProcessors()), false, true);
+            emitClosingBrace(stringBuilder);
+        } else if (object instanceof FileInfo) {
+            FileInfo fileInfo = (FileInfo) object;
+            emitOpeningBrace(stringBuilder);
+            emitKeyValuePair(stringBuilder, "functionName", fileInfo.getFunctionName(), true, false);
+            emitKeyValuePair(stringBuilder, "programmingLanguage", fileInfo.getProgrammingLanguage(), true, true);
+            emitClosingBrace(stringBuilder);
+        } else {
+            throw new RuntimeException("Object is not recongized for the creation of JSON file in the ELEGANT Acceleration Service.");
+        }
+        return stringBuilder.toString();
+    }
+}
