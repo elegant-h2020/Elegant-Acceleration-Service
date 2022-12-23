2af9d9f0d5c3380cb8f11a3cc4400c1953f07ea2
Thanos Stratikopoulos <athanasios.stratikopoulos@manchester.ac.uk>
2022-12-23 21:15:14 +0000

    [refactor] Renaming package and internal environmental variables

--- src/main/java/uk/ac/manchester/elegant/acceleration/service/controller/EnvironmentVariables.java
+++ src/main/java/uk/ac/manchester/elegant/acceleration/service/controller/EnvironmentVariables.java
@@ -1,4 +1,4 @@
-package uk.ac.manchester.acceleration.service.elegant.controller;
+package uk.ac.manchester.elegant.acceleration.service.controller;
 
 public class EnvironmentVariables {
     public static final String JAVA_HOME = "JAVA_HOME";
@@ -6,7 +6,7 @@ public class EnvironmentVariables {
 
     public static final String UPLOADED_DIR = "UPLOADED_DIR";
     public static final String TORNADO_SDK = "TORNADO_SDK";
-    public static final String TORNADOVM_DIR = "TORNADOVM_DIR";
-    public static final String SERVICE_DIR = "SERVICE_DIR";
+    public static final String TORNADOVM_ROOT = "TORNADOVM_ROOT";
+    public static final String SERVICE_HOME = "SERVICE_HOME";
     public static final String BOILERPLATE_DIR = "BOILERPLATE_DIR";
 }
