dc061dca015b7535449cfa568117ba6f3c7fa1e6
Thanos Stratikopoulos <athanasios.stratikopoulos@manchester.ac.uk>
2022-12-23 16:30:36 +0000

    [feat] Made compilation script to be generic

--- bin/runCompilation.sh
+++ bin/runCompilation.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
 
-if [ "$#" -ne 6 ] 
+if [ "$#" -ne 7 ]
 then
         echo "$0: invalid number of arguments."
         exit 1
 fi
 
-class_path=$1
-class_file=$2
-device_json_file=$3
-parameter_size_json_file=$4
-generated_kernel_file=$5
-method_name=$6
+tornado_command=$1
+class_path=$2
+class_file=$3
+device_json_file=$4
+parameter_size_json_file=$5
+generated_kernel_file=$6
+method_name=$7
 
 ## Compile input class file for virtual device 
-/home/thanos/repositories/tornadoVM2/bin/sdk/bin/tornado -cp :$class_path --jvm="-Dtornado.input.classfile.dir=$class_file -Dtornado.device.desc=$device_json_file -Dtornado.parameter.size.dir=$parameter_size_json_file -Dtornado.virtual.device=True -Dtornado.cim.mode=True -Dtornado.print.kernel=True -Dtornado.print.kernel.dir=$generated_kernel_file" uk.ac.manchester.tornado.drivers.opencl.service.frontend.TestFrontEnd --params "$method_name"
+$tornado_command -cp :$class_path --jvm="-Dtornado.input.classfile.dir=$class_file -Dtornado.device.desc=$device_json_file -Dtornado.parameter.size.dir=$parameter_size_json_file -Dtornado.virtual.device=True -Dtornado.cim.mode=True -Dtornado.print.kernel=True -Dtornado.print.kernel.dir=$generated_kernel_file" uk.ac.manchester.tornado.drivers.opencl.service.frontend.TestFrontEnd --params "$method_name"
 
