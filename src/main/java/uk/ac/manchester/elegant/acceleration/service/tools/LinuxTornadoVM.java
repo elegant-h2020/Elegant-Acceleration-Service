4465fcdc5389280dfc331c65db389f136099a49d
Thanos Stratikopoulos <athanasios.stratikopoulos@manchester.ac.uk>
2023-02-14 23:50:00 +0000

    [feat] Update TornadoVM commit point

--- src/main/java/uk/ac/manchester/elegant/acceleration/service/tools/LinuxTornadoVM.java
+++ src/main/java/uk/ac/manchester/elegant/acceleration/service/tools/LinuxTornadoVM.java
@@ -48,7 +48,7 @@ public class LinuxTornadoVM implements TornadoVMInterface {
         ArrayList<String> args = new ArrayList<>();
         args.add(environmentTornadoVM.get(EnvironmentVariables.JAVA_HOME) + "/bin/javac");
         args.add("-cp");
-        args.add(environmentTornadoVM.get(EnvironmentVariables.TORNADOVM_ROOT) + "/dist/tornado-sdk/tornado-sdk-0.15-dev-04f4353/share/java/tornado/tornado-api-0.15-dev.jar");
+        args.add(environmentTornadoVM.get(EnvironmentVariables.TORNADOVM_ROOT) + "/dist/tornado-sdk/tornado-sdk-0.16-dev-43c4c72/share/java/tornado/tornado-api-0.16-dev.jar");
         args.add("-g:vars");
         args.add(environmentTornadoVM.get(EnvironmentVariables.BOILERPLATE_DIR) + "/" + id + "/" + ClassGenerator.getVirtualClassFileName(methodFileName));
         return args.toArray(new String[args.size()]);
