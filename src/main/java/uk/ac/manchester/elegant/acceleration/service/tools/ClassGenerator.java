03ac11b06e82c32c4ed094395f9ca1a078767581
Thanos Stratikopoulos <athanasios.stratikopoulos@manchester.ac.uk>
2023-07-28 11:50:30 +0300

    [style] Add copyright header

--- src/main/java/uk/ac/manchester/elegant/acceleration/service/tools/ClassGenerator.java
+++ src/main/java/uk/ac/manchester/elegant/acceleration/service/tools/ClassGenerator.java
@@ -1,3 +1,22 @@
+/*
+ * This file is part of the ELEGANT Acceleration Service.
+ * URL: https://github.com/elegant-h2020/Elegant-Acceleration-Service.git
+ *
+ * Copyright (c) 2023, APT Group, Department of Computer Science,
+ * The University of Manchester. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package uk.ac.manchester.elegant.acceleration.service.tools;
 
 import java.io.BufferedReader;
@@ -16,8 +35,11 @@ public class ClassGenerator {
 
     private static void emitPackagePrologue(StringBuilder sb) {
         sb.append("import uk.ac.manchester.tornado.api.annotations.Parallel;");
+        sb.append("\n");
         sb.append("import uk.ac.manchester.tornado.api.collections.math.TornadoMath;");
+        sb.append("\n");
         sb.append("import uk.ac.manchester.tornado.api.collections.types.Float2;");
+        sb.append("\n");
         sb.append("import uk.ac.manchester.tornado.api.collections.types.VectorFloat2;");
         sb.append("\n");
     }
