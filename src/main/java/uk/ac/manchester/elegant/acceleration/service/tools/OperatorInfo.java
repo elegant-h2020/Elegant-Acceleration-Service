d8dde6fe99f1343d65209f526681d9fd959b750f
Thanos Stratikopoulos <athanasios.stratikopoulos@manchester.ac.uk>
2023-07-31 11:27:34 +0300

    [wip] Renaming mechanism to transform NES Java UDF input files

---
+++ src/main/java/uk/ac/manchester/elegant/acceleration/service/tools/OperatorInfo.java
@@ -0,0 +1,37 @@
+/*
+ * This file is part of the ELEGANT Acceleration Service.
+ * URL: https://github.com/elegant-h2020/Elegant-Acceleration-Service.git
+ *
+ * Copyright (c) 2023, APT Group, Department of Computer Science,
+ * The University of Manchester. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.ac.manchester.elegant.acceleration.service.tools;
+
+import java.util.ArrayList;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class OperatorInfo {
+    ArrayList<String> listOfOperatorObjectNames = new ArrayList<>();
+    ConcurrentHashMap<String, OperatorObject> inputVariableNameToTypeMap = new ConcurrentHashMap<>();
+    ArrayList<String> argumentNameList = new ArrayList<>();
+    ArrayList<OperatorObject> inputList = new ArrayList<>();
+    ConcurrentHashMap<OperatorObject, String> tornadifiedInputList = new ConcurrentHashMap<>();
+    ArrayList<OperatorObject> outputList = new ArrayList<>();
+    ArrayList<OperatorObject> tornadifiedOutputList = new ArrayList<>();
+    boolean isMethodPublic;
+    boolean isMethodStatic;
+    boolean methodUsesMathPackage;
+    String udfName;
+}
