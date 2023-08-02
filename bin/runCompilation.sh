05c9977c773647f22a3d8519bfcd1e66d0ce24d6
Thanos Stratikopoulos <athanasios.stratikopoulos@manchester.ac.uk>
2023-08-02 15:13:44 +0300

    [feat] Update to return the exit code of the TornadoVM compilation command

--- bin/runCompilation.sh
+++ bin/runCompilation.sh
@@ -14,6 +14,15 @@ parameter_size_json_file=$5
 generated_kernel_file=$6
 method_name=$7
 
-## Compile input class file for virtual device 
+## Compile input class file for virtual device
 $tornado_command -cp :$class_path --jvm="-Dtornado.input.classfile.dir=$class_file -Dtornado.device.desc=$device_json_file -Dtornado.parameter.size.dir=$parameter_size_json_file -Dtornado.virtual.device=True -Dtornado.cim.mode=True -Dtornado.print.kernel=True -Dtornado.print.kernel.dir=$generated_kernel_file" uk.ac.manchester.tornado.drivers.opencl.service.frontend.TestFrontEnd --params "$method_name"
+
+exit_code=$?
+
+if [ $exit_code -e 0 ]
+then
+        exit 0
+else
+        exit $exit_code
+fi
 
