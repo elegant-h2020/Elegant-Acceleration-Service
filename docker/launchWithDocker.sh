2f7484ee5c7bb611497eb04e66cc34e5856a6cf8
Thanos Stratikopoulos <athanasios.stratikopoulos@manchester.ac.uk>
2024-01-08 23:15:23 +0200

    Update docker build script

--- docker/launchWithDocker.sh
+++ docker/launchWithDocker.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
 IMAGE=acceleration-service-container
-TAG_VERSION=1.0
+TAG_VERSION=demo
 
 if [[ "$1" == "--buildAndLaunch" ]]; then
     docker volume create elegant_service_files
     docker build --no-cache -t $IMAGE .
     docker tag $IMAGE beehivelab/$IMAGE:$TAG_VERSION
-    docker tag $IMAGE beehivelab/$IMAGE:latest
-    docker run -it -p 8080:8080 -v "$PWD":/elegant_service_files beehivelab/acceleration-service-container:latest
+    #docker tag $IMAGE beehivelab/$IMAGE:latest
+    docker run -it -p 8080:8080 -v "$PWD":/elegant_service_files beehivelab/acceleration-service-container:$TAG_VERSION
 elif [[ "$1" == "--launch" ]]; then
-    docker run -it -p 8080:8080 -v "$PWD":/elegant_service_files beehivelab/acceleration-service-container:latest
+    docker run -it -p 8080:8080 -v "$PWD":/elegant_service_files beehivelab/acceleration-service-container:$TAG_VERSION
 elif [[ "$1" == "--deleteVolume" ]]; then
     docker volume rm elegant_service_files
 else
